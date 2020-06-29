cedcad8778087fa989f2bce12eaab457cd1e6481
posssible99 <juanpsfi3023@gmail.com>
2020-06-28 21:07:22 -0500

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./components/shop/shop.component";
 import SignInAndSignUpPage from "./components/sign-in-sign-up/sign-in-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -18,13 +18,28 @@ class App extends React.Component {
 
   componentDidMount() {
     // This give us a function that when we call, it closes the suscription(componentWillUnmount)
-    this.unsuscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        // When we call the function crete...(only for googleSignUp), if it's the first time the user sign up, we store the data of the user.
+        // Remember that this function returns userRef, and we need to wait beacuase it's assynchronus
+        const userRef = await createUserProfileDocument(userAuth);
+        // We obtain the snapshot object, where it's conatin the data of the user
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: snapShot.id,
+            // We pass the other properties, like email,etc
+            ...snapShot.data(),
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
   componentWillUnmount() {
+    console.log("desmonte");
     // This closes the suscription
     this.unsuscribeFromAuth();
   }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,35 @@ const config = {
   measurementId: "G-B77Q58GZT9",
 };
 
+// Store our user in the database
+// Take care of the users that uses google and the others that don't
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; //We expect a null.
+
+  //   We get a object with data, but the most important parameter is exists.(this is a document reference)
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  // A snapshShot is a object that conatins data of a doc of a collection
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    //   The user doesn't exist, so we need some info to create our user.
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      // We use await because it's asynchronus, we need to wait the database.
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.messsage);
+    }
+  }
+  //   We return userRef beacuse we maybe use it again
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
